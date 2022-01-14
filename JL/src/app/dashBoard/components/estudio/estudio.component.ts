b1e1b403cd403453fe97c5d136e6b593bf2ac89e
Juan Fonseca <juanpablofonsecac5@gmail.com>
2022-01-14 08:41:09 -0500

    componentes de estudio

--- JL/src/app/dashBoard/components/estudio/estudio.component.ts
+++ JL/src/app/dashBoard/components/estudio/estudio.component.ts
@@ -1,7 +1,6 @@
-import { FlatTreeControl } from '@angular/cdk/tree';
+
 import { Component} from '@angular/core';
-import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
-import { MateriaNode, ExampleFlatNode } from '../../../models/materia';
+
 
 @Component({
   selector: 'app-estudio',
@@ -10,51 +9,5 @@ import { MateriaNode, ExampleFlatNode } from '../../../models/materia';
 })
 export class EstudioComponent {
 
-  TREE_DATA: MateriaNode[] = [
-    {
-      name: 'Fruit',
-      children: [{name: 'Apple'}, {name: 'Banana'}, {name: 'Fruit loops'}],
-    },
-    {
-      name: 'Vegetables',
-      children: [
-        {
-          name: 'Green',
-          children: [{name: 'Broccoli'}, {name: 'Brussels sprouts'}],
-        },
-        {
-          name: 'Orange',
-          children: [{name: 'Pumpkins'}, {name: 'Carrots'}],
-        },
-      ],
-    },
-  ];
 
-    private _transformer = (node: MateriaNode, level: number) => {
-      return {
-        expandable: !!node.children && node.children.length > 0,
-        name: node.name,
-        level: level,
-      };
-    };
-  
-    treeControl = new FlatTreeControl<ExampleFlatNode>(
-      node => node.level,
-      node => node.expandable,
-    );
-
-    treeFlattener = new MatTreeFlattener(
-      this._transformer,
-      node => node.level,
-      node => node.expandable,
-      node => node.children,
-    );
-
-  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-
-  constructor() {
-    this.dataSource.data = this.TREE_DATA;
-  }
-
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 }
---
+++ JL/src/app/dashBoard/components/estudio/examenes-dash/examenes-dash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-examenes-dash',
+  templateUrl: './examenes-dash.component.html',
+  styleUrls: ['./examenes-dash.component.css']
+})
+export class ExamenesDashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
